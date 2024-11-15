2a2df74a9ca3c77ec22cd366614d0a3570219ada
juan <jdelgadolaguna@alumnos.exa.unicen.edu.ar>
2024-11-15 19:12:02 -0300

    arreglado metodo getAll y contexto en router

--- router.php
+++ router.php
@@ -7,8 +7,8 @@
     #                  endpoint     verbo      controller        metodo
     $router->addRoute('peliculas'     , 'GET', 'FilmApiController', 'getAll');
     $router->addRoute('pelicula/:id'  , 'GET', 'FilmApiController', 'get');
-    $router->addRoute('peliculas/:id'  , 'DELETE', 'FilmApiController', 'delete');
-    $router->addRoute('peliculas'  , 'POST', 'FilmApiController', 'create');
-    $router->addRoute('peliculas/:id'  , 'PUT', 'FilmApiController', 'update');
+    $router->addRoute('pelicula/:id'  , 'DELETE', 'FilmApiController', 'delete');
+    $router->addRoute('pelicula'  , 'POST', 'FilmApiController', 'create');
+    $router->addRoute('pelicula/:id'  , 'PUT', 'FilmApiController', 'update');
 
     $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
